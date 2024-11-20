5ea200505f0501a75f227e28cceba50857f31478
EthanRTZ <rietz.ethan@gmail.com>
2024-11-20 18:47:17 +0100

    Initial commit

---
+++ src/main/java/com/example/bot_dis/App.java
@@ -0,0 +1,13 @@
+package com.example.bot_dis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
